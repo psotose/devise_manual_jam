e9b759402a3afabea87bd9497b562c6d69a881d5
patito <p.sotosellanes@hotmail.com>
2020-02-19 10:22:12 -0300

    devise

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,10 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /stories
   # GET /stories.json
   def index
-    @stories = Story.all
+    @stories = current_user.stories
   end
 
   # GET /stories/1
@@ -25,7 +25,7 @@ class StoriesController < ApplicationController
   # POST /stories.json
   def create
     @story = Story.new(story_params)
-
+    @story.user = current_user
     respond_to do |format|
       if @story.save
         format.html { redirect_to @story, notice: 'Story was successfully created.' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   resources :stories
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
